3c3478d397d7ee158fcaedae511880bf8ee3e19e
knastin <knastin@users.noreply.github.com>
2016-01-12 21:45:40 +0300

    Update and rename app-lab2.java to app-lab5.java

---
+++ src/main/java/ru/spb/herzen/ivt3/app-lab5.java
@@ -0,0 +1,24 @@
+package ru.spb.herzen.ivt3;
+
+public class App {
+
+    public int sum ( int a, int b){
+        return a + b;
+    }
+
+    public String sum(String a, String b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public float multiply(float a, float b) {
+        return a * b;
+    }
+
+    public float AX(float x) {
+        return 100/(10 - x);
+    }
+}
